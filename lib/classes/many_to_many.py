4a66092ce8ffbb0d6bdf549a28ceff86981765f7
william <williamwambugu663@gmail.com>
2025-05-23 08:07:56 +0300

    testing  instances

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,123 +2,111 @@ class Coffee:
     def __init__(self, name):
         if not isinstance(name, str):
             raise Exception("Name must be a string")
-        if len(name) < 3:
-            raise Exception("Name must be at least 3 characters")
-        self.name = name
+        if len(name) < 4:
+            raise Exception("Name must be at least 4 characters")
+        self._name = name
 
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, name):
-        if hasattr(self, "_name"):
-            raise Exception("Name cannot be changed")
-        if not isinstance(name, str):
+    def name(self, value):
+        # raise Exception("Cannot change coffee name")
+        if hasattr(self, '_name'):
+            raise Exception("Cannot change coffee name")
+        if not isinstance(value, str):
             raise Exception("Name must be a string")
-        if len(name) < 3:
-            raise Exception("Name must be at least 3 characters")
-        self._name = name
+        if len(value) < 4:
+            raise Exception("Name must be at least 4 characters")
+        self._name = value
+
     def orders(self):
-        return [order.customer for order in Order.all if order.coffee == self]
+        """return all orders for this coffee"""
+        return [order for order in Order.all if order.coffee == self]
 
-        pass
-    
     def customers(self):
-        return (set(order.customer for order in self.orders()))
+        return list(set(order.customer for order in self.orders()))
 
-        pass
-    
     def num_orders(self):
         return len(self.orders())
-        pass
-    
+
     def average_price(self):
-        return sum(order.price for order in self.orders()) / len(self.orders()) if self.orders() else 0
-        pass
+        orders = self.orders()
+        return sum(order.price for order in orders) / len(orders) if orders else 0
+
 
 class Customer:
+    _all = []
+
     def __init__(self, name):
         if not isinstance(name, str):
             raise Exception("Name must be a string")
         if not 3 <= len(name) <= 15:
             raise Exception("Name must be between 3 and 15 characters")
-        self.name = name
-        Customer.all.append(self)
-        
+        self._name = name
+        Customer._all.append(self)
+
     @property
-    def name(self):
+    def name(self):        
         return self._name
 
     @name.setter
-    def name(self, name):
-        if hasattr(self, "_name"):
-            raise Exception("Name cannot be changed")
-        if not isinstance(name, str):
+    def name(self, value):
+        if not isinstance(value, str):
             raise Exception("Name must be a string")
-        if not 3 <= len(name) <= 15:
-            raise Exception("Name must be between 3 and 15 characters")
-        self._name = name
-        
+        if not 3 <= len(value) <= 15:
+            raise Exception("Name must be between 1 and 15 characters")
+        self._name = value
+
     def orders(self):
         return [order for order in Order.all if order.customer == self]
-        pass
-    
+
     def coffees(self):
-        return (set(order.coffee for order in self.orders()))
-        pass
-    
+        return list(set(order.coffee for order in self.orders()))
+
     def create_order(self, coffee, price):
         if not isinstance(coffee, Coffee):
-            raise Exception("Coffee must be an instance of Coffee")
-        if not isinstance(price, float):
-            raise Exception("Price must be a float")
-        if not 1.0 <= price <= 10.0:
-            raise Exception("Price must be between 1.0 and 10.0")
-        order = Order(self, coffee, price)
-        Order.all.append(order)
-        return order
-        pass
+            raise Exception("Must be a Coffee instance")
+        return Order(self, coffee, price)
+
     @classmethod
-    def get_all(cls):
-        return cls.all
-    
+    def most_aficionado(cls, coffee):
+        if not isinstance(coffee, Coffee):
+            raise Exception("Must be a Coffee instance")
+        customer_spending = {}
+        for order in coffee.orders():
+            customer_spending[order.customer] = customer_spending.get(order.customer, 0) + order.price
+        return max(customer_spending.items(), key=lambda x: x[1])[0] if customer_spending else None
+
+
 class Order:
     all = []
+
     def __init__(self, customer, coffee, price):
         if not isinstance(customer, Customer):
-            raise Exception("Customer must be an instance of Customer")
+            raise Exception("Customer must be a Customer instance")
         if not isinstance(coffee, Coffee):
-            raise Exception("Coffee must be an instance of Coffee")
-        if not isinstance(price, float):
-            raise Exception("Price must be a float")
-        if not 1.0 <= price <= 10.0:
-            raise Exception("Price must be between 1.0 and 10.0")
-        self.customer = customer
-        self.coffee = coffee
-        self.price = price
+            raise Exception("Coffee must be a Coffee instance")
+        if not isinstance(price, (int, float)) or not 1.0 <= float(price) <= 10.0:
+            raise Exception("Price must be a number between 1.0 and 10.0")
+        self._customer = customer
+        self._coffee = coffee
+        self._price = float(price)
         Order.all.append(self)
 
     @property
     def price(self):
-         return self._price
+        return self._price
 
     @price.setter
-    def price(self, price):
-        if not isinstance(price, float):
-            raise Exception("Price must be a float")
-        if not 1.0 <= price <= 10.0:
-            raise Exception("Price must be between 1.0 and 10.0")
-        self._price = price
-
-        pass
+    def price(self, value):
+        raise Exception("Cannot change price")
 
     @property
     def customer(self):
         return self._customer
 
-    @customer.setter
-    def customer(self, customer):
-        if not isinstance(customer, Customer):
-            raise Exception("Customer must be an instance of Customer")
-        self._customer = customer
+    @property
+    def coffee(self):
+        return self._coffee
